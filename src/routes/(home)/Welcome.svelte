1ce027c5c9869127d7d633b6001a31c35aae453f
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-12 13:12:15 +0000

    recast button spacing

--- src/routes/(home)/Welcome.svelte
+++ src/routes/(home)/Welcome.svelte
@@ -2,9 +2,10 @@
   import { tutorsConnectService } from "$lib/services/connect.svelte";
 </script>
 
-<p class="text-2xl">
-  {#if tutorsConnectService.tutorsId.value?.name}
-    Connected as: {tutorsConnectService.tutorsId.value?.name}
+{#if tutorsConnectService.tutorsId.value?.name}
+  <span class="text-base">Connected as:</span>
+  <p class="text-2xl">
+    {tutorsConnectService.tutorsId.value?.name}
     ({tutorsConnectService.tutorsId.value?.login})
-  {/if}
-</p>
+  </p>
+{/if}
